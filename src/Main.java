9241f4cd11b0e10577c27fb551fce7f66524c496
Willian Talles <willian.talles@outlook.com>
2019-06-02 19:15:18 -0300

    adicição da classe main

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int tipoArbitragem = 0;
+		
+		tipoArbitragem = exibirMenu();
+		System.out.println(tipoArbitragem);
+	}
+	
+	private static int exibirMenu() {
+		Scanner scanner = new Scanner(System.in);
+		int opcao = 0;
+		
+		do {
+			System.out.println("Escolha o tipo de arbitragem:");
+			System.out.println("1- Daisy Chaining.");
+			System.out.println("2- Prioridade Fixa.");
+			System.out.println("3- Prioridade Rotativa.");
+			System.out.println("4- Prioridade justa.");
+			
+			opcao = Integer.parseInt(scanner.nextLine());
+			
+		}while(verificarEscolha(opcao));
+		
+		return opcao;
+		
+	}
+	
+	private static boolean verificarEscolha(int escolha) {
+		if (escolha < 1) {
+			return true;
+		} else if ( escolha > 4) {
+			return true;
+		}
+		
+		return false;
+	}
+
+}
